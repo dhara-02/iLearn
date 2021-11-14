1ef56415022a62afd77e90f830faeba9c3d27683
varun-m-0310 <varun.m@capillarytech.com>
2021-11-14 10:23:00 +0530

    Tab Url added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
   function getMetas() {
-    var message = document.querySelector('#metaTable');
+    // var message = document.querySelector('#metaTable');
     chrome.tabs.executeScript(null, {
-      code: `var metas = document.getElementsByTagName('meta'); 
+      code: `var metas = window.getElementsByTagName('meta'); 
       var metaArr = [];
       for (var i=0; i<metas.length; i++) { 
         var name = metas[i].getAttribute("name");
@@ -26,11 +27,26 @@ function App() {
     }, function() {
       // If you try it into an extensions page or the webstore/NTP you'll get an error
       if (chrome.runtime.lastError) {
-        message.innerText = 'There was an error : \n' + chrome.runtime.lastError.message;
+        // message.innerText = 'There was an error : \n' + chrome.runtime.lastError.message;
       }
     });
   }
-  window.onload = getMetas;
+
+  function getTabChange() {
+    chrome.runtime.sendMessage({
+      method:"onTabChange",
+    });  
+  }
+  function addListener() {
+    chrome.tabs.onActivated.addListener(
+      getMetas
+    )
+  }
+  // window.onload = addListener;
+  useEffect(() => {
+    getTabChange()
+    addListener()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
